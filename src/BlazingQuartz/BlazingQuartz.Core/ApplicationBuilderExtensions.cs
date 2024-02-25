5d73214e7729a8c6a0d562cf5e39f0f7f974dac3
Camillo Cardoso Borges <camillo.cborges@bomgado.com>
2024-02-25 14:20:17 -0300

    Chage Constants to Core and set ApplicationBuildExtensions insted of BlazroQuartzUIBuilderExtensions

---
+++ src/BlazingQuartz/BlazingQuartz.Core/ApplicationBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using BlazingQuartz.Core.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace BlazingQuartz.Core
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseBlazingQuartz(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var options = scope.ServiceProvider.GetRequiredService<IOptions<BlazingQuartzCoreOptions>>().Value;
+                if (options.AutoMigrateDb)
+                {
+                    var db = scope.ServiceProvider.GetRequiredService<BlazingQuartzDbContext>();
+                    db.Database.Migrate();
+                }
+            }
+
+            return app;
+        }
+    }
+}
